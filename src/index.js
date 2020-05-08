20e77747cb2cc1bf7eae12671545a3e9d70cc712
kanishka mittal <kanishkamittal27@gmail.com>
2020-05-08 21:59:29 +0530

    signup and login created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import LoginComponent from './login/login'
+import SignupComponent from './signup/signup'
+import Dashboard from './dashboard/dashboard'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firebase = require('firebase')
+require('firebase/firestore')
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBzOEcsaJ45dzLzndWe_HyvNXqhojmPvSY",
+    authDomain: "react-chat-6b08b.firebaseapp.com",
+    databaseURL: "https://react-chat-6b08b.firebaseio.com",
+    projectId: "react-chat-6b08b",
+    storageBucket: "react-chat-6b08b.appspot.com",
+    messagingSenderId: "159525446202",
+    appId: "1:159525446202:web:1bff612eb8b93c8c29cb03",
+    measurementId: "G-HC8BSBTSYR"
+
+})
+
+const routing = (
+  <Router>
+    <div id="routing-container">
+      <Route path="/login" component={LoginComponent}></Route>
+      <Route path="/signup" component={SignupComponent}></Route>
+      <Route path="/dashboard" component={Dashboard}></Route>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
